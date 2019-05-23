fb596853fcd187d71113e507f9f480819a9f55c8
Cecile Kwan Ning <cecile.kwanning@gmail.com>
2019-05-23 17:44:57 +0200

    Controllers created

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,17 @@
+class BoatsController < ApplicationController
+  def index
+    @boats = Boat.all
+  end
+
+  def show
+    @boat = Boat.find(params[:id])
+  end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    raise
+  end
+end
