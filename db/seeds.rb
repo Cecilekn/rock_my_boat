9067ce479deb5a81e1c3724448baf22ae49f2f99
Cecile Kwan Ning <cecile.kwanning@gmail.com>
2019-05-23 18:56:01 +0200

    Cloudinary ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
+User.destroy_all
+Boat.destroy_all
+
+puts "Detroyed"
+
 leonardo = User.create!(
   first_name: "Leonardo",
   last_name: "Di Caprio",
   email: "leonardo@bg.com",
   password: "12345678"
   )
+
+puts "Leo created"
 
 bollore = User.create!(
   first_name: "Vincent",
@@ -21,6 +28,8 @@ bollore = User.create!(
   password: "12345678"
   )
 
+puts "Bollore created"
+
 jagger = User.create!(
   first_name: "Mick",
   last_name: "Jagger",
@@ -28,15 +37,27 @@ jagger = User.create!(
   password: "12345678"
   )
 
-names = ["Dune 3", "Lili", "Will Be Fine", "Marcaro", "Wolfy", "Pyjama", "Amandine", "Guigui", "Cece", "Beauty of the See"]
+puts "Jagger created"
 
-names.each do |name|
-  boat = Boat.create!(
-    name: name,
+
+array = [
+  ["Dune 3", "https://res.cloudinary.com/di4pxxpr8/image/upload/v1558622355/Rock%20My%20Boat/catamaran-5294_renw9z.jpg"],
+  ["Lili", "https://res.cloudinary.com/di4pxxpr8/image/upload/v1558622332/Rock%20My%20Boat/catamaran-2800_hoytl7.jpg"],
+  ["Will Be Fine", "https://res.cloudinary.com/di4pxxpr8/image/upload/v1558621900/Rock%20My%20Boat/fantasy-1_rc2byl.jpg"],
+  ["Marcaro", "https://res.cloudinary.com/di4pxxpr8/image/upload/v1558620703/Rock%20My%20Boat/touringitaly-rent-a-yatch_moshzk.jpg"],
+  ["Wolfy", "https://res.cloudinary.com/di4pxxpr8/image/upload/v1558621184/Rock%20My%20Boat/46d1a096d37130f762120e96788867b7_exz1sq.jpg"]
+]
+
+array.each do |item|
+  boat = Boat.new(
+    name: item[0],
     capacity: rand(2..50),
-    location: ["Saint Malo", "Nice", "Cannes", "La Ciota", "La Grande Motte", "Palavas les Flots"].sample,
+    location: ["Saint Malo", "Nice", "Cannes", "La Ciotat", "La Grande Motte", "Palavas les Flots"].sample,
     theme: ["Pirate", "James Bond", "Gatsby", "Peter Pan", "Woodstock"].sample,
     price_per_day: rand(200..2000),
-    user_id: rand(1..3)
+    user_id: rand(1..3),
   )
+  puts "boat sans photo"
+  boat.remote_photo_url = item[1]
+  boat.save
 end
