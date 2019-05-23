344fe160b5f1777f67de32eb217e43a8402cb05f
Cecile Kwan Ning <cecile.kwanning@gmail.com>
2019-05-23 18:09:33 +0200

    Setting up photo uploader

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -12,6 +12,19 @@ class BoatsController < ApplicationController
   end
 
   def create
-    raise
+    @boat = Boat.new(boat_params)
+    @user = current_user
+    @boat.user_id = @user.id
+    if @boat.save
+      redirect_to boat_path(@boat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name, :capacity, :location, :theme, :price_per_day, :photo)
   end
 end
--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,11 @@
 class Boat < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  THEMES = ["Pirate", "James Bond", "Gatsby", "Peter Pan", "Woodstock"]
+
+  validates :name, :capacity, :location, :theme, :price_per_day, :photo, presence: true
+  validates :theme, inclusion: { in: THEMES }
+
+  mount_uploader :photo, PhotoUploader
 end
