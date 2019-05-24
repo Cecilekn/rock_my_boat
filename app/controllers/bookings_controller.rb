4c4c4b2d93d1e2f0123fd65b85aa89e7d5ca5a14
Cecile Kwan Ning <cecile.kwanning@gmail.com>
2019-05-24 16:25:04 +0200

    BookingsController ready

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,20 +7,44 @@ class BookingsController < ApplicationController
   def bookings_owner
     @user = current_user
     @boats = @user.boats
+    @bookings = []
+    @boats.each do |boat|
+      boat.bookings.each do |booking|
+        @bookings << booking
+      end
+    end
+    @bookings
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def new
+    @booking = Booking.new
+    @boat = Boat.find(params[:boat_id])
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @boat = Boat.find(params[:boat_id])
+    @booking.boat = @boat
+    @booking.user = current_user
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render 'bookings/new'
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,24 @@
 class ReviewsController < ApplicationController
   def new
+    @review = Review.new
+    @booking = Booking.find(params[:booking_id])
   end
 
   def create
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new(review_params)
+    @review.booking = @booking
+    @review.user = current_user
+    if @review.save
+      redirect_to my_bookings_path
+    else
+      render 'reviews/new'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :note)
   end
 end
