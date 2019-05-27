4c39da5057218d8e8033256477f2ce6d14132936
MarieAmandine Chevalier <chevaliermac@gmail.com>
2019-05-27 14:43:47 +0200

    add validates on boat form and autocompute

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,9 +4,13 @@ class Boat < ApplicationRecord
   has_many :reviews, through: :bookings
 
   THEMES = ["Pirate", "James Bond", "Gatsby", "Peter Pan", "Woodstock"]
+  CAPACITY = [2, 5, 10, 15, 20, 30, 40, 50]
+  PRICE = (0..2000)
 
   validates :name, :capacity, :location, :theme, :price_per_day, :photo, presence: true
   validates :theme, inclusion: { in: THEMES }
+  validates :capacity, inclusion: { in: CAPACITY }
+  validates :price_per_day, inclusion: { in: PRICE }
 
   mount_uploader :photo, PhotoUploader
 end
