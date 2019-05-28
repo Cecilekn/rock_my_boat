58e05316d3d8f01b24d2126ad69ce7a701431478
Celia Heidsieck <celia.heidsieck@gmail.com>
2019-05-28 15:33:29 +0200

    modif de pages controller pour les markers de la map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @boats = Boat.where.not(latitude: nil, longitude: nil)
+    @markers = @boats.map do |boat|
+      {
+        lat: boat.latitude,
+        lng: boat.longitude
+      }
+    end
   end
 end
