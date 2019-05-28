a3658879e09a1d1fc69a01227ca6d502c4095783
Celia Heidsieck <celia.heidsieck@gmail.com>
2019-05-28 18:47:44 +0200

    ajout markers + autocomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,15 @@ import { initAutocomplete } from '../plugins/init_autocomplete';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 
-initAutocomplete();
-
 if (document.querySelector(".banner")) {
+  console.log("from app.js banner")
   const navbar = document.querySelector(".navbar");
     navbar.classList.add("navbar-transparent");
 };
 
-
-
 initMapbox();
+initAutocomplete();
+
+
+
+
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,6 +1,7 @@
 import mapboxgl from 'mapbox-gl';
 
 const initMapbox = () => {
+
   const mapElement = document.getElementById('map');
 
   const fitMapToMarkers = (map, markers) => {
@@ -9,12 +10,24 @@ const initMapbox = () => {
     map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
   };
 
+  const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
+      .addTo(map);
+    });
+  };
+
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
       new mapboxgl.Marker()
@@ -22,7 +35,10 @@ const initMapbox = () => {
         .addTo(map);
     });
     fitMapToMarkers(map, markers);
-  }
+    addMarkersToMap(map, markers);
+  };
+
+
 };
 
 export { initMapbox };
