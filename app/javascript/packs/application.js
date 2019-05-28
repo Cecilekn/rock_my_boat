4ec4a14317b224cdb1a5037af062581d03471f56
Celia Heidsieck <celia.heidsieck@gmail.com>
2019-05-28 12:52:22 +0200

    recherche avancée

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,7 @@ import "bootstrap";
 import { initAutocomplete } from '../plugins/init_autocomplete';
 initAutocomplete();
 
+if (document.querySelector(".banner")) {
+  const navbar = document.querySelector(".navbar");
+    navbar.classList.add("navbar-transparent");
+};
