a1bed3ca2f8f4691934c8fa69a01007a7307fdb1
Cecile Kwan Ning <cecile.kwanning@gmail.com>
2019-05-28 14:03:24 +0200

    First set up.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_150840) do
+ActiveRecord::Schema.define(version: 2019_05_28_112552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2019_05_24_150840) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
