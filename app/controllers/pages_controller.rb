b5623e91d940e6fb05c2f5b431934673df439cbb
Celia Heidsieck <celia.heidsieck@gmail.com>
2019-05-29 12:18:58 +0200

    navbar au scroll + markers sur la map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,8 @@ class PagesController < ApplicationController
       {
         lat: boat.latitude,
         lng: boat.longitude,
-        infoWindow: render_to_string(partial: "infowindow", locals: { boat: boat })
+        infoWindow: render_to_string(partial: "infowindow", locals: { boat: boat }),
+        image_url: helpers.asset_url('https://res.cloudinary.com/di4pxxpr8/image/upload/v1558621184/Rock%20My%20Boat/46d1a096d37130f762120e96788867b7_exz1sq.jpg')
       }
     end
   end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,15 @@ import { initAutocomplete } from '../plugins/init_autocomplete';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 import { initSweetalert } from '../plugins/init_sweetalert';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+if (document.querySelector(".banner") === null) {
+  console.log('hello')
+  document.querySelector('.navbar-lewagon').classList.add('navbar-blue');
+};
 
 if (document.querySelector(".banner")) {
-  console.log("from app.js banner")
-  const navbar = document.querySelector(".navbar");
-    navbar.classList.add("navbar-transparent");
+  initUpdateNavbarOnScroll();
 };
 
 initMapbox();
